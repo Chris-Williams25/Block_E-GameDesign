74918fcbc2369afdf9e24d91f5c6694518b3de06
Your Name <willscgreen2022@gmail.com>
2022-01-20 14:20:26 -0600

    homework

---
+++ Homework 1/Homework_1.20.2022.py
@@ -0,0 +1,16 @@
+import os 
+os.system('cls')
+
+#part one
+
+print("Greenhill : 10, Portal : 5.33")
+print("Total students : 240, Boys : 120")
+print("031")
+print("3.561E+02")
+
+#part two
+
+print("Welcome to", end=" ")
+print("Greenhill School")
+#part three
+print('I\nLove\t"Greenhill\"')
