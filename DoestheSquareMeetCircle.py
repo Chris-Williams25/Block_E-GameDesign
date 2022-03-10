802de9196173b74df349bd30998d29bc3db67eba
Chris-Williams25 <willscgreen2022@gmail.com>
2022-03-09 21:05:40 -0600

    thishere

--- DoestheSquareMeetCircle.py
+++ DoestheSquareMeetCircle.py
@@ -1,19 +1,6 @@
-#MAria I SUarez
-#learning how to draw circles and rectangles
-#use keys to move objects
-#Using Dictionaries
-
-#Objective of the game is for the rect to run away fom the circle, if they collide the circle etas the square, 
-#circle will  get larger, and a new rect should appear somewhere on the screen
-# K_UP                  up circle
-# K_DOWN                down circle
-# K_RIGHT               right circle
-# K_LEFT                left circle
-# K_a                   left square
-# K_d                   right square
-# K_w                   up square
-# K_s                   down square
-#initialize pygame
+#I copied and pasted this, and then I added the parameters to keep the sqaure in the box frame thing
+#chris Williams
+#3/9/2022
 import os, random, time, pygame
 #initialize pygame
 pygame.init()
